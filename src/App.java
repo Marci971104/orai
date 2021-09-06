c07d8c4dc859507a6bfb5e8390e8909e5bace4ed
Rohrbacher Marcell <rohrbacher.marcell97@gmail.com>
2021-09-06 12:19:56 +0200

    Házi előtt

--- src/App.java
+++ src/App.java
@@ -6,28 +6,13 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
         aboutPrint();
-        //Fejrész kiírása
-        System.out.println("Jelszavak");
-        //Verzió kiírása
-        System.out.println("Verzió: 0.0.1");
+        Bekeres();
+        int Success = 0; 
         
         //Az a objektummal kérhetünk be a konzolról
-        Scanner scan = new Scanner(System.in);
-
-        System.out.print("Felhasználónév: ");
-        // A b változó tárolja a fnév
-        String Name = scan.nextLine(); 
-        System.out.print("Jelszó: ");                                           //Nem jó megnevezés//
-        String Pw = scan.nextLine();
-        System.out.print("Hely: ");
-        String Place = scan.nextLine();
-        scan.close();
-        int Success = 0;                                                             //Interface megjelölése nem helyes//
-        try {
-            /* 
-            A jelszó, a felhasználónév és a 
-            használati helye a profile 
-            objektumban van tárolva                    */
+        
+    
+        try {                  */
             Profile profile = new Profile(Name, Pw, Place);
             FileWriter fileWriter = new FileWriter("pass.txt");
             PrintWriter printWriter = new PrintWriter(fileWriter);
@@ -46,8 +31,25 @@ public class App {
 
     }
 
+
+    public static void Bekeres(){
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Felhasználónév: ");
+        String Name = scan.nextLine(); 
+        System.out.print("Jelszó: ");                                           //Nem jó megnevezés//
+        String Pw = scan.nextLine();
+        System.out.print("Hely: ");
+        String Place = scan.nextLine();
+        scan.close();
+
+    }
+
+
     public static void aboutPrint() {
         System.out.println("Nagy János");
+        System.out.println("Jelszavak");
+        //Verzió kiírása
+        System.out.println("Verzió: 0.0.1");
 
     }
 }
