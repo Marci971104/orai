35614c3c709c2b89f3f5db4d15ff658bb7a572c4
Rohrbacher Marcell <rohrbacher.marcell97@gmail.com>
2021-09-06 10:36:54 +0200

    Kész

--- src/App.java
+++ src/App.java
@@ -3,49 +3,49 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Scanner;
 
-public class App {
+public class App {//
     public static void main(String[] args) {
-        nevjegy();
+        NameCard();
         //Fejrész kiírása
         System.out.println("Jelszavak");
         //Verzió kiírása
         System.out.println("Verzió: 0.0.1");
         
         //Az a objektummal kérhetünk be a konzolról
-        Scanner a = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
 
         System.out.print("Felhasználónév: ");
-        // A b változó tárolja a jelszót
-        String b = a.nextLine();
-        System.out.print("Jelszó: ");
-        String c = a.nextLine();
+        // A b változó tárolja a fnév
+        String Name = scan.nextLine();
+        System.out.print("Jelszó: ");                                                //Nem jó megnevezés//
+        String Pw = scan.nextLine();
         System.out.print("Hely: ");
-        String d = a.nextLine();
-        a.close();
-        int iSiker = 0;
+        String Place = scan.nextLine();
+        scan.close();
+        int Success = 0;                                                             //Interface megjelölése nem helyes//
         try {
             /* 
             A jelszó, a felhasználónév és a 
             használati helye a passList 
             objektumban van tárolva            
             */
-            Store passList = new Store(b, c, d);
-            FileWriter f = new FileWriter("pass.txt");
-            PrintWriter pwr = new PrintWriter(f);
+            Store passList = new Store(Name, Pw, Place);
+            FileWriter fwrite = new FileWriter("pass.txt");
+            PrintWriter pwr = new PrintWriter(fwrite);
             pwr.print(passList.user);
             if(!passList.hollow()) { pwr.print(passList.retrieval()); }
             pwr.print(passList.place);
             pwr.close();
-            iSiker = 1;
-        } catch (IOException e) {
+           Success = 1;
+        } catch (IOException exc) {
             System.err.println("Hiba! A fájlbaírás sikertelen. Keresse meg a fejlesztőt.");
         }
 
-        if(iSiker == 1) { System.out.println("Ok. A kiírás sikeres.");  }else {  System.out.println("Hiba! A kiírás sikertelen"); }
+        if(Success == 1) { System.out.println("Ok. A kiírás sikeres.");  }else {  System.out.println("Hiba! A kiírás sikertelen"); }
 
     }
 
-    public static void nevjegy() {
+    public static void NameCard() {
         System.out.println("Nagy János");
     }
 }
